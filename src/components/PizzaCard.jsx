e925b80c77aa3daff60129bb036fb11eddb2e755
itsdrews <vavr.snf24@uea.edu.br>
2025-08-11 12:25:37 -0400

    Fixed PizzaCard.jsx

--- src/components/PizzaCard.jsx
+++ src/components/PizzaCard.jsx
@@ -5,34 +5,35 @@ const PizzaCard = ({ pizza }) => {
   return (
     <div className="pizza-card">
       <img  className='pizza-image' src='/pizza-piece.svg'></img>
+
+      <div className="pizza-info">
+
       <div className="pizza-header">
         <h2>{pizza.nome}</h2>
+          </div>
         <p className="pizza-ingredients">{ingredientesString}</p>
          <div className="pizza-footer">
-        <div className="pizza-prices">
           {Object.entries(pizza.valores).map(([tamanho, valor],index) => (
             <div key={tamanho} className="price-item">
-                <div className="inside-separator">
-                {index > 0 && <div className="price-separator"></div>}
-              <div className="size-price">
+            {index > 0 && <div className="price-separator"></div>}
+               <div className="size-price">
+
               <span className="size">{tamanho}:</span>
               <span className="price">R$ {valor.toFixed(2)}</span>
+
               <div className="cart-image-container">
                <span><img src="/carrinho.svg" alt="cart" className='cart-image' /></span>
               </div>
-              </div>
 
+               </div>
                 </div>
-              </div>
-              
-
               
         
           ))}
-        </div>
+       
+      </div>
       </div>
        
-          </div>
     
      
     </div>
