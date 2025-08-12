008250c6c349d54edf84862db1c6c30a6129725a
itsdrews <vavr.snf24@uea.edu.br>
2025-08-12 09:17:05 -0400

    New Comanda

--- src/pages/Comanda.jsx
+++ src/pages/Comanda.jsx
@@ -32,7 +32,15 @@ const Comanda = () => {
       quantidade: 4
     }
   ];
- 
+ const calcularTotal = (comanda) =>{
+
+  const total = comanda.reduce(
+    (total,item) => total + (item.valor * item.quantidade),0
+  );
+  
+  return total
+
+ }// ##To do: Container da mesa e lógica de comanda
   return (
     <>
     <Header></Header>
@@ -42,7 +50,20 @@ const Comanda = () => {
           <PizzaCard key={pizza.id} pizza={pizza} cardMode={"comanda"}/>
         ))}
       </div>
-
+      <div className="delivery-type-container">
+        <div className="table">
+          <h2>Mesa</h2>
+          <p>Número da mesa</p>
+          <p>{comanda.mesa}</p>
+        </div>
+        <div className="delivery">
+          <h2>Entrega</h2>
+          <p>Endereço</p>
+          <label htmlFor="">Endereço de entrega <input className='address-input-container'  placeholder='Digite o endereço de entrega'type="text" /> </label>
+         
+        </div>
+      </div>
+      <p className='total'>Total: R$ {calcularTotal(comanda)}</p>
   
     </div>
     <Footer></Footer>
